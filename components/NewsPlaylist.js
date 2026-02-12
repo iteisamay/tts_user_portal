bb02e03a1407e963fe1350c0fb53e31c71610df7
ArkadeepChakraborty <arkadeepchakra2003@gmail.com>
2026-02-12 19:49:20 +0530

    update margins
    Some Margins Updated

--- components/NewsPlaylist.js
+++ components/NewsPlaylist.js
@@ -353,7 +353,7 @@ const NewsPlaylist = ({ id, language, description, title, audioUrl, thumbnail, d
                 </motion.div>
 
                 <motion.div
-                    className="flex items-center justify-center gap-4 sm:gap-6 mt-2"
+                    className="flex items-center justify-center gap-4 sm:gap-6 mt-4 -mb-1"
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.5 }}
