3dbb42e0fd1b6f31e43f0ada3800b1637b483684
aaligerd <amerpoth@gmail.com>
2026-02-25 02:25:31 +0530

    Changing the google tag manager to google analytics manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProviderWrapper from "./theme-provider";
-import { GoogleTagManager } from '@next/third-parties/google'
-
-
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,18 +14,21 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Eisamay:Listen our audio new",
-  description: "Stay updated with the latest Bengali news on politics, sports, business, entertainment, and more. Read trusted Bangla news from West Bengal&#x27;s leading newspaper, Ei Samay.",
+  title: "Eisamay: Listen our audio news",
+  description:
+    "Stay updated with the latest Bengali news on politics, sports, business, entertainment, and more.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <GoogleTagManager gtmId="G-G6WE90DNWD" />
       <body className={`${geistSans.variable} ${geistMono.variable} transition-colors`}>
         <ThemeProviderWrapper>
           {children}
         </ThemeProviderWrapper>
+
+        {/* ✅ GA4 */}
+        <GoogleAnalytics gaId="G-G6WE9XXXXD" />
       </body>
     </html>
   );
