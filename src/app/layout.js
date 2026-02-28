23f286ee33cc1cc77310ab2c8b5adfbe8f4e80e5
ArkadeepChakraborty <it@eisamay.com>
2026-02-28 14:45:54 +0530

    update layout js
    Update Layout JS

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,28 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_DOMAIN),
+
   title: "Eisamay: Listen our audio news",
   description:
     "Stay updated with the latest Bengali news on politics, sports, business, entertainment, and more.",
+
+  manifest: "/manifest.json",
+
+  themeColor: "#d40000",
+
+  icons: {
+    icon: [
+      { url: `${process.env.NEXT_PUBLIC_DOMAIN}/listen/eisamay.png` },
+      { url: `${process.env.NEXT_PUBLIC_DOMAIN}/listen/eisamay.png`, sizes: "32x32", type: "image/png" },
+    ],
+    shortcut: `${process.env.NEXT_PUBLIC_DOMAIN}/listen/eisamay.png`,
+  },
+
+  openGraph: {
+    type: "website",
+    siteName: "Eisamay",
+  },
 };
 
 export default function RootLayout({ children }) {
